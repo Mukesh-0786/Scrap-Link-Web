65e69c368e9cb176fee24be42bcdf11c54d269f6
Mukesh-0786 <mukeshkumars@student.tce.edu>
2025-12-02 11:35:33 +0530

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,6 @@
+module.exports = function(grunt) {
+  // Minimal Gruntfile so CI can run `grunt` without errors
+  grunt.initConfig({});
+  // Default task does nothing
+  grunt.registerTask('default', []);
+};
